ef3c1ca2f2c00c6afa440816a9a82e35b837de7c
Bendegreef <ben.de.greef@student.ehb.Be>
2015-05-20 11:36:19 +0200

    js veranderd

--- www/js/script.js
+++ www/js/script.js
@@ -12,32 +12,27 @@ document.addEventListener("deviceready", onDeviceReady, false);
 // Cordova is ready to be used!
 //
 function onDeviceReady() {
-	console.log(window.device);
-	console.log(window.plugins);
-	alert('device ready');
-	document.getElementById("takePicture").onclick = takePicture;
+   console.log(window.device);
+   console.log(window.plugins);
+    alert('device ready');
+    document.getElementById("takePicture").onclick = takePicture();
 }
 
-function takePicture() {
-	navigator.camera.getPicture(function (imageURI) {
-			alert(imageURI);
-		},
-		function (message) {
-			alert(message);
-		}, {
-			quality: 50,
-			destinationType: Camera.DestinationType.FILE_URI
-		});
+function takePicture(e) {
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50,
+        destinationType: navigator.camera.DestinationType.DATA_URL
+    });
 }
 
 function onSuccess(imageData) {
-	var image = document.getElementById('myImage');
-	image.src = "data:image/jpeg;base64," + imageData;
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
 }
 
 function onFail(message) {
-	setTimeout(function () {
-		//alert('Failed because: ' + message);
-	}, 0);
+    setTimeout(function () {
+        //alert('Failed because: ' + message);
+    }, 0);
 
 }
